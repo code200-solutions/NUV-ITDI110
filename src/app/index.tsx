0879fc55dfe9750cce69797daf880e7f83005c5e
Angele Jacquier <angele@code200.solutions>
2025-08-28 16:18:17 +1100

    Update: style post containers

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -11,10 +11,32 @@ export default function HomeScreen() {
         paddingHorizontal: 40,
         height: '100%',
       }}>
-      <View>
+      <View
+        style={{
+          backgroundColor: '#F3F4F6',
+          marginBottom: 16,
+          padding: 16,
+          borderRadius: 12,
+          shadowColor: '#000',
+          shadowOpacity: 0.05,
+          shadowRadius: 4,
+          elevation: 2,
+          width: '100%',
+        }}>
         <Text>My First Post</Text>
       </View>
-      <View>
+      <View
+        style={{
+          backgroundColor: '#F3F4F6',
+          marginBottom: 16,
+          padding: 16,
+          borderRadius: 12,
+          shadowColor: '#000',
+          shadowOpacity: 0.05,
+          shadowRadius: 4,
+          elevation: 2,
+          width: '100%',
+        }}>
         <Text>My Second Post</Text>
         <Image
           source={{
@@ -23,7 +45,18 @@ export default function HomeScreen() {
           style={{ width: 200, height: 200, borderRadius: 12 }}
         />
       </View>
-      <View>
+      <View
+        style={{
+          backgroundColor: '#F3F4F6',
+          marginBottom: 16,
+          padding: 16,
+          borderRadius: 12,
+          shadowColor: '#000',
+          shadowOpacity: 0.05,
+          shadowRadius: 4,
+          elevation: 2,
+          width: '100%',
+        }}>
         <Image
           source={{
             uri: 'https://plus.unsplash.com/premium_photo-1673292293042-cafd9c8a3ab3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlfGVufDB8fDB8fHww',
