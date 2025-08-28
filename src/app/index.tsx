0681a08ccd2565469540932173f69afbb093d1b7
Angele Jacquier <angele@code200.solutions>
2025-08-28 16:16:11 +1100

    Update: Add image to third post container

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -16,7 +16,14 @@ export default function HomeScreen() {
           style={{ width: 200, height: 200, borderRadius: 12 }}
         />
       </View>
-      <View></View>
+      <View>
+        <Image
+          source={{
+            uri: 'https://plus.unsplash.com/premium_photo-1673292293042-cafd9c8a3ab3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlfGVufDB8fDB8fHww',
+          }}
+          style={{ width: 200, height: 200, borderRadius: 12 }}
+        />
+      </View>
     </View>
   );
 }
