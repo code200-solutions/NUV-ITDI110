0e1520a732884ca7f42aaecfdf52affbc16a3f78
Angele Jacquier <angele@code200.solutions>
2025-08-28 12:06:51 +1100

    Add: First commit

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function HomeScreen() {
+  return (
+    <div className='flex flex-1'>
+      <div>
+        <p>Welcome to NUV...</p>
+      </div>
+    </div>
+  );
+}
