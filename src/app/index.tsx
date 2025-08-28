d9ae143f84f4ccf037fcdf2bb3df34ff31f814b0
Angele Jacquier <angele@code200.solutions>
2025-08-28 21:47:33 +1100

    Update: Add text and/or imageUri prop values for each post

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -13,9 +13,12 @@ export default function HomeScreen() {
         paddingHorizontal: 40,
         height: '100%',
       }}>
-      <Post />
-      <Post />
-      <Post />
+      <Post text='My First Post' />
+      <Post
+        text='My Second Post'
+        imageUri='https://images.unsplash.com/photo-1509042239860-f550ce710b93'
+      />
+      <Post imageUri='https://plus.unsplash.com/premium_photo-1673292293042-cafd9c8a3ab3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlfGVufDB8fDB8fHww' />
     </View>
   );
 }
