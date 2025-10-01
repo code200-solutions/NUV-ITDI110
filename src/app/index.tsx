ba4b4c9f25eb7c306772b4ccfdd56186a1f54edc
ju3100 <junior.kavick@univ.edu.vu>
2025-10-01 11:42:46 +1100

    update: post

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,64 +2,106 @@ import React, { useState } from 'react';
 import { View, Text, ScrollView, TextInput, Pressable, ImageSourcePropType } from 'react-native';
 import PostComponent from './PostComponent';
 
-type NewPost = {image: ImageSourcePropType; content: string; author: string};
+// type NewPost = {image: ImageSourcePropType; content: string; author: string};
+class PostData {
+  author:string;
+  content:string;
+  image: ImageSourcePropType;
+  date : string;
+
+  constructor(author: string, content: string, image:ImageSourcePropType, date:string){
+    this.author = author;
+    this.content = content;
+    this.image = image;
+    this.date = date;
+
+  }
+}
 const MyImage = require('./../../assets/images/12.jpg')
 
 
 export default function HomeScreen() {
-  let postList: NewPost[] = [];
-  const [newPost, setNewPost] = useState<NewPost[]>([]);
-  const [content, setContent] =useState<string>('');
-  const [author, setAuthor] = useState<string>('');
+  let postList: PostData[] = [];
+  const [newPost, setNewPost] = useState<PostData[]>([]);
+  const [author, setAuthor] =useState<string>('');
+  const [content, setContent] = useState<string>('');
+  
 
 
   function onPressFunction(){
+    const today = new Date().toLocaleDateString();
+    const post = new PostData(author, content, MyImage,today)
     //console.log('Hello')
     // postList.push({
     //   image : MyImage,
     //   content:'hey there'
     
     // });
-    setNewPost((prev) => [...prev, {
-      image: MyImage,
-      content: content,
-      author: author
+    setNewPost((prev) => [...prev, post])
+    
+      setAuthor('');
+      setContent('');
+      
 
-    }]);
-    //console.log(postList);
-  }
+    }
+
+    // function postToDelete(index:number){
+    //   setNewPost((prev) => prev.filter((_, i) => i! == index));
+    // }
+    // //console.log(postList);
+  
 
   return (
-    <View>
+    <View style={{backgroundColor:'#f1e8edff'}}>
           <Text style={{ fontWeight: 'bold', fontSize: 10, margin: 5 }}>POST</Text>
     
      <View>
-      <View style={{flexDirection:'row'}}>
-        <Text>Content</Text>
-        <TextInput placeholder='TEXT' value={content} onChangeText={setContent}/>
+      <View style={{flexDirection:'row', margin:5}}>
+        <Text style={{justifyContent:'space-between',borderWidth:1}}>Content</Text>
+
+          <View style={{justifyContent:'space-between', paddingLeft:10}}>
+            <TextInput placeholder='TEXT' value={content} onChangeText={setContent} />
+          </View>
+        
       </View>
 
-        <View style={{ flexDirection: 'row' }}>
-          <Text>Author</Text>
-          <TextInput placeholder='TEXT' value={author} onChangeText={setAuthor} />
+        <View style={{ flexDirection: 'row', margin:5}}>
+          <Text style={{justifyContent:'space-between', borderWidth:1,}}>Author</Text>
+
+          <View style={{justifyContent:'space-between',paddingLeft:10 }}>
+            <TextInput placeholder='TEXT' value={author} onChangeText={setAuthor} />
+          </View>
+          
 
           <TextInput/>
 
         </View>
 
-        <Pressable onPress={onPressFunction}>
-          <Text>ADD</Text>
+        <Pressable 
+            onPress={onPressFunction}>
+              
+              <Text style={{color:'red', borderWidth:1, borderColor:'gray', borderStyle:"dotted", paddingLeft:10
+
+          }}>ADD</Text>
         </Pressable>
-        
+
+        {/* <Pressable onPress={()=>postToDeleteost(index)}>
+          <Text style={{ color: 'red', borderWidth: 1, borderColor: 'gray', borderStyle: "dotted" }}>DELETE</Text>
+        </Pressable>
+         */}
 
      </View>
     
      <ScrollView style={{ height: 520, flex: 1 }}>
-        {newPost && newPost.map((post) => (<View key={post.content}><PostComponent Author= {post.author} Date='15 -09-2025'
+        {newPost && newPost.map((post,index) => (<View key={index}>
+          <PostComponent 
+          Author= {post.author}
+          Date={post.date}
           ImageUrl={post.image}
           Content={post.content}
 
-        /></View>))}
+        /></View>
+        ))}
         {/* {postList && postList.map((post) => (<PostComponent Author='Tim' Date='15 -09-2025'
           ImageUrl={post.image}
           Content={post.content}
