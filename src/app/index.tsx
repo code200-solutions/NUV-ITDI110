ef6c8fb7eac58500a1cf46c3a76f9fb2d56ce321
ju3100 <junior.kavick@univ.edu.vu>
2025-10-08 09:26:14 +1100

    add image space

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,12 +4,12 @@ import PostComponent from './PostComponent';
 
 // type NewPost = {image: ImageSourcePropType; content: string; author: string};
 class PostData {
-  author:string;
-  content:string;
+  author: string;
+  content: string;
   image: ImageSourcePropType;
-  date : string;
+  date: string;
 
-  constructor(author: string, content: string, image:ImageSourcePropType, date:string){
+  constructor(author: string, content: string, image: ImageSourcePropType, date: string) {
     this.author = author;
     this.content = content;
     this.image = image;
@@ -23,56 +23,56 @@ const MyImage = require('./../../assets/images/12.jpg')
 export default function HomeScreen() {
   let postList: PostData[] = [];
   const [newPost, setNewPost] = useState<PostData[]>([]);
-  const [author, setAuthor] =useState<string>('');
+  const [author, setAuthor] = useState<string>('');
   const [content, setContent] = useState<string>('');
-  const [imageUrl,setImage]= useState("");
-
-  
+  const [imageUrl, setImage] = useState("");
 
 
-  function onPressFunction(){
+
+
+  function onPressFunction() {
     const today = new Date().toLocaleDateString();
-    const post = new PostData(author, content, MyImage,today)
+    const post = new PostData(author, content, MyImage, today)
 
     setNewPost((prev) => [...prev, post])
-    
-      setAuthor('');
-      setContent('');
-      setImage('');
-      
 
-    }
+    setAuthor('');
+    setContent('');
+    setImage('');
 
-    // function postToDelete(index:number){
-    //   setNewPost((prev) => prev.filter((_, i) => i! == index));
-    // }
-    // //console.log(postList);
-  
+
+  }
+
+  // function postToDelete(index:number){
+  //   setNewPost((prev) => prev.filter((_, i) => i! == index));
+  // }
+  // //console.log(postList);
+
 
   return (
-    <View style={{backgroundColor:'#f1e8edff', flex:1}}>
-          <Text style={{ fontWeight: 'bold', fontSize: 10, margin: 5 }}>POST</Text>
-    
-     <View>
-      <View style={{flexDirection:'row', margin:5,}}>
-        <Text style={{justifyContent:'space-between',borderWidth:1, paddingRight:5}}>Content</Text>
+    <View style={{ backgroundColor: '#f1e8edff', flex: 1 }}>
+      <Text style={{ fontWeight: 'bold', fontSize: 10, margin: 5 }}>POST</Text>
 
-          <View style={{justifyContent:'space-between', borderWidth:1,paddingLeft:5,marginLeft:10}}>
+      <View>
+        <View style={{ flexDirection: 'row', margin: 5, }}>
+          <Text style={{ justifyContent: 'space-between', borderWidth: 1, paddingRight: 5 }}>Content</Text>
+
+          <View style={{ justifyContent: 'space-between', borderWidth: 1, paddingLeft: 5, marginLeft: 10 }}>
             <TextInput placeholder='write something' value={content} onChangeText={setContent} />
           </View>
-        
-      </View>
 
-        <View style={{ flexDirection: 'row', margin:5}}>
-          <Text style={{justifyContent:'space-between', borderWidth:1,}}>Author</Text>
+        </View>
 
-          <View style={{justifyContent:'space-between',paddingLeft:10,borderWidth:1, marginLeft:22 }}>
-          
+        <View style={{ flexDirection: 'row', margin: 5 }}>
+          <Text style={{ justifyContent: 'space-between', borderWidth: 1, }}>Author</Text>
+
+          <View style={{ justifyContent: 'space-between', paddingLeft: 10, borderWidth: 1, marginLeft: 22 }}>
+
             <TextInput placeholder='your name' value={author} onChangeText={setAuthor} />
           </View>
-    
+
         </View>
-        
+
         <View style={{ flexDirection: 'row', margin: 5 }}>
           <Text style={{ justifyContent: 'space-between', borderWidth: 1, }}>Url Image</Text>
 
@@ -82,38 +82,39 @@ export default function HomeScreen() {
           </View>
 
         </View>
-        
 
-        <Pressable 
-            onPress={onPressFunction}>
 
-              
-              <Text style={{color:'red', borderWidth:1, borderColor:'gray', 
-              borderStyle:"solid",alignSelf:'flex-start',
-              fontWeight:'500', 
-              fontSize:12, margin:5, 
+        <Pressable
+          onPress={onPressFunction}>
+
+
+          <Text style={{
+            color: 'red', borderWidth: 1, borderColor: 'gray',
+            borderStyle: "solid", alignSelf: 'flex-start',
+            fontWeight: '500',
+            fontSize: 12, margin: 5,
 
           }}>ADD</Text>
         </Pressable>
 
-       
 
-     </View>
-    
-     <ScrollView style={{ flex: 1 }}>
-        {newPost && newPost.map((post,index) => (<View key={index}>
-          <PostComponent 
-          Author= {post.author}
-          Date={post.date}
-          ImageUrl={post.image}
-          Content={post.content}
 
-        />
+      </View>
+
+      <ScrollView style={{ flex: 1 }}>
+        {newPost && newPost.map((post, index) => (<View key={index}>
+          <PostComponent
+            Author={post.author}
+            Date={post.date}
+            ImageUrl={post.image}
+            Content={post.content}
+
+          />
         </View>
         ))}
-        
-      
-     </ScrollView>
+
+
+      </ScrollView>
     </View>
 
 
